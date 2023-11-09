8bce953b29819e1a7479d6489876a94152230f06
Aleksandr <145176583+AlexPop69@users.noreply.github.com>
2023-11-09 17:12:31 +0800

    Add files via upload

---
+++ 1816. Truncate Sentence.go
@@ -0,0 +1,40 @@
+package main
+
+import "strings"
+
+//A sentence is a list of words that are separated by a single space
+//with no leading or trailing spaces. Each of the words consists of only
+//uppercase and lowercase English letters (no punctuation).
+
+//For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+//You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains
+//only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+//Variant - 1. Using count of space
+func truncateSentence(s string, k int) string {
+	countSpace := 0
+	toK := 0
+	for i, v := range s {
+		if v == ' ' {
+			countSpace++
+		}
+		if countSpace == k {
+			toK = i
+			return s[0:toK]
+		}
+	}
+	return s
+}
+
+//Variant - 2. Using slice
+func truncateSentence1(s string, k int) string {
+	result := strings.Split(s, " ")
+	s1 := ""
+	for i, v := range result {
+		if i == k {
+			break
+		}
+		s1 += v + " "
+	}
+	return s1[:len(s1)-1]
+}
