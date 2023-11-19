4603b5a98a9f9c1351e25e992463a28c412658ec
Aleksandr <145176583+AlexPop69@users.noreply.github.com>
2023-11-19 10:59:18 +0800

    Update 27. Remove Element.go

--- 27. Remove Element.go
+++ 27. Remove Element.go
@@ -1,16 +1,29 @@
 package main
 
-import "sort"
+import (
+	"sort"
+)
+
+//Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+//The order of the elements may be changed.
+//Then return the number of elements in nums whichare not equal to val.
+
+//Consider the number of elements in nums which are not equal to val be k, to get accepted,
+//you need to do the following things:
+
+//Change the array nums such that the first k elements of nums contain
+//the elements which are not equal to val. The remaining elements of nums are not important
+//as well as the size of nums.
+//Return k.
 
 func removeElement(nums []int, val int) int {
-	count := len(nums)
+	k := len(nums)
 	for i, v := range nums {
 		if v == val {
-			nums[i] = 101
-			count--
+			nums[i] = 1000
+			k--
 		}
 	}
-	//sort.Slice(nums, func(i,j int) bool {return nums[i] < nums[j]})
 	sort.Ints(nums)
-	return count
+	return k
 }
