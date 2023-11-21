c3a792af316e6aef4918009891f8d3a753c178d3
Aleksandr <145176583+AlexPop69@users.noreply.github.com>
2023-11-21 15:55:16 +0800

    Update 66. Plus One.go

--- 66. Plus One.go
+++ 66. Plus One.go
@@ -3,18 +3,16 @@ package main
 import (
 	"math/big"
 	"strconv"
-	"strings"
 )
 
 func plusOne(digits []int) []int {
-	var s []string
+	str := ""
 	for i := 0; i < len(digits); i++ {
-		s = append(s, strconv.Itoa(digits[i]))
+		a := strconv.Itoa(digits[i])
+		str += a
 	}
 
-	text := strings.Join(s, "")
-
-	intD, _ := new(big.Int).SetString(text, 10)
+	intD, _ := new(big.Int).SetString(str, 10)
 
 	plusOne := new(big.Int).SetInt64(1)
 	intD = intD.Add(intD, plusOne)
